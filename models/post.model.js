d1c1c74975844a2411d533193ba0650e82359500
서유진 <50348197+yujiniii@users.noreply.github.com>
2022-09-07 00:39:45 +0900

    feat: ORM모델링

---
+++ models/post.model.js
@@ -0,0 +1,39 @@
+const Sequelize = require("sequelize");
+const DataTypes = Sequelize.DataTypes;
+
+const Post = (sequelize) =>
+  sequelize.define(
+    "Post",
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      weather: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      charset: "utf8mb4",
+      collate: "utf8mb4_unicode_ci",
+      timestamps: true,
+      paranoid: true, // 논리삭제
+      underscored: true,
+    }
+  );
+
+module.exports = Post;
