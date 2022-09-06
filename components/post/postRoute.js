3ec60107d5691441e4868f37d23a3f2db0f88ce8
서유진 <50348197+yujiniii@users.noreply.github.com>
2022-09-07 01:40:10 +0900

    feat: 게시글 등록 api 작성(중)

--- components/post/postRoute.js
+++ components/post/postRoute.js
@@ -15,3 +15,25 @@ router.get("/post", async (req, res, next) => {
     next(err);
   }
 });
+
+// 게시글 등록
+router.post("/post", async (req, res, next) => {
+  try {
+    const { title, content, password, userId } = req.body;
+    if (postService.newPasswordAuth(password)) {
+      const weather = postService.findWeather(userId);
+      const posted = await postService.postPosted(
+        title,
+        content,
+        password,
+        userId,
+        weather
+      );
+      res.status(200).json({ posted });
+    } else {
+      res.status(500).json({ message: "비밀번호 규정을 지켜주세요" });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
